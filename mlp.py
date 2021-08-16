0fa9583ddc6c6aa9bc21c2a13489113323e4744a
Govind <govindagarwal24@gmail.com>
2021-08-16 15:12:22 +0530

    model built, compiled and trained using tensorflow library.

--- mlp.py
+++ mlp.py
@@ -13,8 +13,8 @@ def generate_dataset(num_samples, test_size=0.3):
 
     Returns:
         x_train (ndarray): 2d array with input data for training
-        y_train (ndarray): 2d array with target data for training
         x_test (ndarray):  2d array with input data for testing
+        y_train (ndarray): 2d array with target data for training
         y_test (ndarray):  2d array with target data for testing
     """
     # build inputs and targets
@@ -22,12 +22,27 @@ def generate_dataset(num_samples, test_size=0.3):
     y = np.array([[i[0]+i[1]] for i in x])
 
     # split into training and testing datasets
-    x_train, y_train, x_test, y_test = train_test_split(
+    x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=test_size)
 
-    return x_train, y_train, x_test, y_test
+    return x_train, x_test, y_train, y_test
 
 
 if __name__ == "__main__":
-    print("here")
-    x_train, y_train, x_test, y_test = generate_dataset(5000, 0.3)
+    # create dataset with 500 samples
+    x_train, x_test, y_train, y_test = generate_dataset(10000, 0.3)
+
+    # build model with 3 layers: 2 -> 5 -> 1
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(5, input_dim=2, activation="sigmoid"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    # choose optimiser
+    optimiser = tf.keras.optimizers.SGD(learning_rate=0.05)
+
+    # compile model
+    model.compile(optimizer=optimiser, loss="mse")
+
+    # train model
+    model.fit(x_train, y_train, epochs=100)
